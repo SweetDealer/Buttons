de5b008bff87520fea7067855e3317edccecc2e3
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-01-23 22:05:25 +0200

    Added dropdown menu-list

--- js/button.js
+++ js/button.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.button-split__right, .button-split__dropdown a').forEach((button) => {
+document.querySelectorAll('.button-split__right, .button-split__dropdown li').forEach((button) => {
     button.addEventListener('click', () => {
         button.closest('.button-split').classList.toggle('button-split--open')
         console.log(1)
