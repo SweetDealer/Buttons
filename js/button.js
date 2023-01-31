f26be5cccc6e72fbf05c03652bc2764b074319d4
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-01-31 22:15:37 +0200

    Changed class names of buttons in button-group

--- js/button.js
+++ js/button.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.button-group--right, .dropdown-menu li').forEach((button) => {
+document.querySelectorAll('.button-group--right, .menu-dropdown li').forEach((button) => {
     button.addEventListener('click', () => {
         button.closest('.button-with-dropdown').classList.toggle('button-group--open')
         console.log(1)
